2ec1dbaa04620c6f2b7970bdba0f0d164ab9dd60
Oli Hickie <oli@thethinkingtraveller.com>
2023-08-31 16:04:06 +0100

    initial commit

---
+++ kids-games/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Rhys & Zach</h1>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
+
+<style lang="scss"></style>
