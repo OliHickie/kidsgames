5eada61a11aa6b180e32389c9c42bae5c46435ab
Oli Hickie <oli@thethinkingtraveller.com>
2023-09-01 16:46:19 +0100

    Add colours content for pairs

---
+++ kids-games/src/content/colours.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    id: 1,
+    name: "Green circle",
+    image:
+      "https://uxwing.com/wp-content/themes/uxwing/download/signs-and-symbols/green-circle-icon.png",
+  },
+  {
+    id: 2,
+    name: "Blue triangle",
+    image:
+      "https://www.pngitem.com/pimgs/m/285-2850728_transparent-blue-triangle-png-triangle-png-download.png",
+  },
+  {
+    id: 3,
+    name: "Red square",
+    image:
+      "https://illustoon.com/photo/9211.png",
+  },
+  {
+    id: 4,
+    name: "Purple hexagon",
+    image:
+      "https://www.seekpng.com/png/detail/279-2797423_purple-hexagon-hexagon-shape-purple.png",
+  },
+  {
+    id: 5,
+    name: "Orange rectangle",
+    image:
+      "https://clipart-library.com/image_gallery/57109.png",
+  },
+  {
+    id: 6,
+    name: "Pink diamond",
+    image:
+      "https://www.clker.com/cliparts/W/r/B/s/L/W/pink-diamond-hi.png",
+  },
+  {
+    id: 7,
+    name: "Yellow Trapezium",
+    image: "https://t3.ftcdn.net/jpg/05/73/54/12/360_F_573541296_gph0FVs0JxEQPzAxqc7PRM5uOSvFTgLR.jpg",
+  },
+  {
+    id: 8,
+    name: "Aqua Decagon",
+    image:
+      "https://www.theschoolrun.com/sites/theschoolrun.com/files/u9/regular_decagon.png",
+  },
+];
