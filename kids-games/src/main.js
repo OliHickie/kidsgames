28445b7f72b88d091a8600f164b47a4b5d9b06b2
Oli Hickie <oli@thethinkingtraveller.com>
2023-09-01 11:35:21 +0100

    Create deck and shuffle

--- kids-games/src/main.js
+++ kids-games/src/main.js
@@ -5,4 +5,9 @@ import store from "./store";
 
 import "@/assets/_main.scss";
 
+if (process.env.NODE_ENV === 'production') {
+  app.config.devtools = false; // Disable Vue Devtools
+  app.config.productionTip = false; // Disable development mode warnings
+}
+
 createApp(App).use(store).use(router).mount("#app");
--- kids-games/src/views/PairsView.vue
+++ kids-games/src/views/PairsView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pairs">
     <div class="pairs__container">
-      <div v-for="(card, i) in content" :key="i">
+      <div v-for="(card, i) in shuffledContent" :key="i">
         <div class="card" :style="getBackground(card)" />
       </div>
     </div>
@@ -10,29 +10,41 @@
 
 <script>
 import Marvel from '../content/marvel';
+import { ref, computed, onMounted, onBeforeUnmount, watch } from 'vue';
 
 export default {
-  data() {
-    return {
-      content: [...Marvel.characters, ...Marvel.characters],
-    }
-  },
-  methods: {
-    getBackground(card) {
+  setup() {
+    const content = Array(4).fill(Marvel.characters).flat();
+
+    const shuffledContent = computed(() => {
+      const copyContent = [...content];
+      for (let i = copyContent.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [copyContent[i], copyContent[j]] = [copyContent[j], copyContent[i]];
+      }
+      return copyContent;
+    });
+
+    const getBackground = (card) => {
       return {
         backgroundImage: `url(${card.image})`,
-      }
-    }
-  }
-}
+      };
+    };
 
+    return {
+      shuffledContent,
+      getBackground,
+    };
+  },
+};
 </script>
 
 <style scoped lang="scss">
-
 .pairs {
   &__container {
     display: flex;
+    align-items: center;
+    justify-content: center;
     max-width: 90vw;
     flex-wrap: wrap;
     margin: 0 auto;
@@ -40,13 +52,14 @@ export default {
 }
 .card {
   background-color: $blue;
-  height: 30vh;
-  width: 20vh;
+  height: 20vh;
+  width: 15vh;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
   border-radius: 20px;
   margin-right: 16px;
   margin-bottom: 16px;
+  // border: 1px solid grey;
 }
 </style>
